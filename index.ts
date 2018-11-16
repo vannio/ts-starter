0c5e244ae9527dff71a72ab741cddd8f3a4532e0
Van <vannio@users.noreply.github.com>
2018-11-16 16:43:32 +0000

    Initialise bull queues + envs

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config({});
 
 app.listen(3000, () => {
   console.log("Hi, I'm hogging port 3000");
