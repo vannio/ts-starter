5702a7f0e290fe4c4327c0171737d40a34f489fa
Van <notify@vann.io>
2018-11-19 15:01:53 +0000

    Fixed tests (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-const express = require('express');
-// const app = express();
-const request = require('request');
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
+import express, { Request, Response} from 'express';
+import request from 'request-promise-native';
+
+const app = express();
 dotenv.config({});
 
 const giphyKey = process.env.GIPHY_API_KEY;
 const giphyUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${giphyKey}`;
-request(giphyUrl, (err, res, body) => console.log('body:', body));
 
-// app.listen(3000, () => {
-//   console.log("Hi, I'm hogging port 3000");
-// });
+app.get('/', (req: Request, res: Response) => {
+  request(giphyUrl).then((result: Response) => res.send(result));
+});
+
+app.listen(3000, () => {
+  console.log("Hi, I'm hogging port 3000");
+});
+
+export default app;
---
+++ tests/index.test.ts
@@ -0,0 +1,20 @@
+import nock from 'nock';
+import request, { Response } from 'supertest';
+
+describe('index.ts', () => {
+  nock('https://api.giphy.com')
+    .get('/v1/gifs/trending')
+    .query(true)
+    .reply(200, require('./fixtures/giphy-trending.json'));
+
+  describe('GET /api.giphy.com/v1/gifs/trending', () => {
+    test('should return 200', async () => {
+      const app = require('../src/index');
+      return request(app)
+        .get('/')
+        .then((response: Response) => {
+          return expect(response.status).toBe(200);
+        });
+    });
+  });
+});
